42a3c18e99b838e4794bc4a8178d8c820a17cbb9
Ana Luiza Pfeilsticker de Oliveira Araújo Tristão <anapfeilsticker@gmail.com>
2023-03-28 08:51:43 -0300

    individualmovie

--- my_film_list/src/pages/Movie.jsx
+++ my_film_list/src/pages/Movie.jsx
@@ -1,15 +1,38 @@
 
+import { useState, useEffect} from "react";
+import { useParams } from "react-router-dom";
+import MovieCard from "../components/MovieCard";
 
 
 
-
-
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 function Movie() {
 
+  const{id} = useParams()
+  const [movie, setMovie] = useState(null)
+
+
+  const getMovie = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setMovie(data);
+};
+
+useEffect(() => {
+  const movieUrl = `${moviesURL}${id}?${apiKey}`;
+
+  getMovie(movieUrl)
+});
+
+
     return (
       <div className="App">
-        <h1>MyFilmList</h1>
+
+        {movie && (
+          <><MovieCard movie={movie} showLink={false}/></>
+        )}
       </div>
     )
   }
